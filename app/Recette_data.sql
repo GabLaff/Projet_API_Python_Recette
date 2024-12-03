435717f94ec20997351609ee77d387e0c9cd8cd6
Gabriel Lafrance <gablafrance14@gmail.com>
2024-12-03 14:08:26 -0500

    Autre commit

--- app/Recette_data.sql
+++ app/Recette_data.sql
@@ -2,14 +2,18 @@ INSERT INTO Recette (nom, ingredient) VALUES
 ('Poulet au beurre', 'Poulet, Beurre, Oignon, Ail, Gingembre, Crème épaisse, Sauce tomate, Garam masala, Cumin'),
 ('Gâteau au chocolat', 'Chocolat, Farine, Sucre, Beurre, Oeufs, Levure chimique, Sel'),
 ('Lasagne végétarienne', 'Pâtes à lasagne, Sauce tomate, Fromage ricotta, Épinards, Fromage mozzarella, Courgettes, Ail'),
-('Pizza Margherita', 'Pâte à pizza, Sauce tomate, Mozzarella, Basilic frais, Huile d'/'olive, Sel'),
+('Pizza Margherita', 'Pâte à pizza, Sauce tomate, Mozzarella, Basilic frais, Huile dolive, Sel'),
 ('Curry de pois chiches', 'Pois chiches, Lait de coco, Pâte de curry, Tomates concassées, Épinards, Oignon, Ail, Gingembre'),
 ('Salade César', 'Laitue romaine, Croûtons, Parmesan, Poulet grillé, Sauce César, Anchois, Citron'),
-('Soupe à l'/'oignon', 'Oignon, Bouillon de boeuf, Vin blanc, Beurre, Farine, Pain, Fromage gruyère'),
+('Soupe à loignon', 'Oignon, Bouillon de boeuf, Vin blanc, Beurre, Farine, Pain, Fromage gruyère'),
 ('Tacos au poisson', 'Tortillas, Poisson, Chou rouge, Crème aigre, Coriandre, Citron vert, Piment jalapeño'),
-('Crêpes sucrées', 'Farine, Sucre, Lait, Oeufs, Beurre, Vanille, Sirop d'/'érable'),
+('Crêpes sucrées', 'Farine, Sucre, Lait, Oeufs, Beurre, Vanille, Sirop dérable'),
 ('Riz sauté aux légumes', 'Riz, Carottes, Petits pois, Oignons verts, Sauce soja, Oeufs, Huile de sésame, Gingembre');
 
+
+INSERT Into User (email, password) VALUES
+                                       ('admin@gmail.com','admin'),
+                                       ('gab@gmail.com', 'gab2024');
 SELECT *
 FROM Recette
 WHERE ingredient LIKE '%Poulet%';
